ed9f540d000b9d578478b108622bd3b3496977e6
zhaohuimin <eezhao10@163.com>
2020-12-04 17:50:00 +0800

    netty

---
+++ src/main/java/com/leo/zhao/netty/NettyServer.java
@@ -0,0 +1,46 @@
+package com.leo.zhao.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyServer {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        /*  boosGroup
+         *  workerGroup
+         *  serverBootstrap  设置配置
+         * */
+        EventLoopGroup boosGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(boosGroup, workerGroup)//设置两个线程组
+                    .channel(NioServerSocketChannel.class)//使用  NioServerSocketChannel 作为服务器的通道实现
+                    .option(ChannelOption.SO_BACKLOG, 128)//设置最大连接数为 128
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)//设置保持活动连接状态
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        //设置处理器
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new NettyServerHandler());
+                        }
+                    }); // 给workerGroup的eventLoop对应的管道设置处理器
+            System.out.println("....... 服务器 is ready .................");
+
+            //绑定一个端口,并启动服务器,生成一个future对象
+            ChannelFuture cf = serverBootstrap.bind(6668).sync();
+            //对关闭的通道尽心监听
+            cf.channel().closeFuture().sync();
+        } finally {
+            boosGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/leo/zhao/netty/NettyServerHandler.java
@@ -0,0 +1,31 @@
+package com.leo.zhao.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("server ctx = " + ctx);
+        ByteBuf byteBuf = (ByteBuf) msg;
+        System.out.println("客户端发来的消息是:" + byteBuf.toString(CharsetUtil.UTF_8));
+        System.out.println("客户端地址是" + ctx.channel().remoteAddress());
+    }
+
+    //数据读取完毕
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        //相应客户端
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello,客户端~", CharsetUtil.UTF_8));
+    }
+
+    //出异常关闭
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
